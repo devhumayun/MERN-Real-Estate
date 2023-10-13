74f29ce2db31d8d713b4a30e3189b9485c4d6142
devhumayun <humayunkabir9408@gmail.com>
2023-10-13 18:41:28 +0600

    connect database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { connectMongoDB } from './config/db.js'
+dotenv.config()
 
 const app = express()
 
 app.listen(5050, () => {
     console.log("Server is runing for 5050 port");
+    connectMongoDB()
 })
