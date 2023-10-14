9952ccbd2dd4a1dc55bd395bd6e024f294da1ba8
devhumayun <humayunkabir9408@gmail.com>
2023-10-14 09:34:11 +0600

    create a error handler middleware

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import { connectMongoDB } from './config/db.js'
 dotenv.config()
 import authRoute from './route/authRoute.js'
+import errorHandler from './middleware/errorHandler.js'
 
 const app = express()
 app.use(express.json());
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended : false }));
 
 // routing
 app.use('/api/v1/auth', authRoute );
+
+// error middleware
+app.use(errorHandler)
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is runing for ${process.env.SERVER_PORT} port`);
