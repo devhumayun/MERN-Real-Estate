4280e2b643908b83dd5704eaa01e57828547031f
devhumayun <humayunkabir9408@gmail.com>
2023-10-18 13:58:52 +0600

    user data refresh problem fix

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,30 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import userSlice from './user/userSlice.js'
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+import userSlice from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+const perosistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(perosistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false
-  })
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
