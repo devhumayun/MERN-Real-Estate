8e309c2a82e0bbb3c075d49c016515487414bd6a
devhumayun <humayunkabir9408@gmail.com>
2023-10-24 00:11:04 +0600

    complate header search funtionlity

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,9 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {BsSearchHeart} from 'react-icons/bs'
 import {useSelector} from 'react-redux'
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const {curentUser} = useSelector(state => state.user)
+  const [searchTerm, setSearchTerm] = useState("")
+  const navigate = useNavigate()
+
+  const handleSearchTerm = (e) => {
+    e.preventDefault()
+
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set("searchTerm", searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const searchingQuery = urlParams.get("searchTerm")
+    if(searchingQuery){
+      setSearchTerm(searchingQuery)
+    }
+  }, [location.search])
+
   return (
     <>
       <div className='w-full shadow-lg bg-gray-300 bg-opacity-90'>
@@ -12,9 +33,9 @@ export default function Header() {
                 <span className="text-slate-600"> Humayun </span>
                 <span className="text-slate-700">Estate </span>
             </div>
-            <form className="flex bg-slate-100 justify-between items-center p-3 rounded-lg ">
-                <input type="text"  placeholder="Search..." className="bg-transparent outline-none w-32 sm:w-60"/>
-                <BsSearchHeart className="cursor-pointer"/>
+            <form onSubmit={handleSearchTerm} className="flex bg-slate-100 justify-between items-center p-3 rounded-lg ">
+                <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search..." className="bg-transparent outline-none w-32 sm:w-60"/>
+                <button><BsSearchHeart className="cursor-pointer"/></button>
             </form>
             <div>
                 <ul className="flex justify-center items-center gap-5 text-slate-700 font-semibold">
